059d74788430b6a37b2202fc99f42684b10863b6
Dan McCabe <mccabe.daniel.p@gmail.com>
2018-05-07 13:28:19 -0400

    Added plus and minus operators for OffsetCoordinate

--- src/com/darkgravity/khexgrid/math/OffsetCoordinate.kt
+++ src/com/darkgravity/khexgrid/math/OffsetCoordinate.kt
@@ -1,5 +1,6 @@
 package com.darkgravity.khexgrid.math
 
+import com.badlogic.gdx.math.GridPoint2
 import com.darkgravity.khexgrid.map.HexagonalOrientation.FlatTop
 import com.darkgravity.khexgrid.map.HexagonalOrientation.PointyTop
 
@@ -12,6 +13,14 @@ data class OffsetCoordinate(val x: Int = 0, val y: Int = 0, val offsetType: Offs
             x - if (offsetType.orientation == PointyTop) adjustCube(y) else 0,
             y - if (offsetType.orientation == FlatTop) adjustCube(x) else 0
         )
+
+    operator fun plus(coordinate: OffsetCoordinate) = OffsetCoordinate(x + coordinate.x, y + coordinate.y, offsetType)
+
+    operator fun plus(point: GridPoint2) = OffsetCoordinate(x + point.x, y + point.y, offsetType)
+
+    operator fun minus(coordinate: OffsetCoordinate) = OffsetCoordinate(x - coordinate.x, y - coordinate.y, offsetType)
+
+    operator fun minus(point: GridPoint2) = OffsetCoordinate(x - point.x, y - point.y, offsetType)
 
     private fun adjustCube(value: Int) = (value + (value and 1) * (if (offsetType.isOdd) -1 else 1)) / 2
 
