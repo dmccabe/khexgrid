3dd3102f5eaceb8da30964a7d501982bf7891ca9
Dan McCabe <mccabe.daniel.p@gmail.com>
2022-09-08 23:59:48 -0400

    Added abliity to set outline color

--- src/com/darkgravity/khexgrid/render/TileOutlineLayer.kt
+++ src/com/darkgravity/khexgrid/render/TileOutlineLayer.kt
@@ -1,5 +1,6 @@
 package com.darkgravity.khexgrid.render
 
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.darkgravity.khexgrid.map.HexagonalTile
@@ -8,14 +9,19 @@ import com.darkgravity.khexgrid.map.HexagonalTile
  * @author Dan McCabe
  */
 class TileOutlineLayer(
-    private val hexagonalRenderer: HexagonalRenderer, shapeRenderer: ShapeRenderer,
+    private val hexagonalRenderer: HexagonalRenderer,
+    shapeRenderer: ShapeRenderer,
+    private val color: Color = Color.BLACK,
     private val filterBlock: ((HexagonalTile) -> Boolean)? = null
 ) : ShapeRendererLayer(shapeRenderer) {
 
     override fun handleRender(batch: PolygonSpriteBatch, tiles: Collection<HexagonalTile>) {
+        val originalColor = shapeRenderer.color
+        shapeRenderer.color = this.color
         (filterBlock?.let { tiles.filter(it) } ?: tiles)
             .forEach {
                 hexagonalRenderer.renderOutline(shapeRenderer, it.location, 5f)
             }
+        shapeRenderer.color = originalColor
     }
 }
